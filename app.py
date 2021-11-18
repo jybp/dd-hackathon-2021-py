0a86e3db5e00051e3ade732c101e249d69179e99
Jean-Baptiste Pinalie <2850825+jybp@users.noreply.github.com>
2021-11-18 15:13:37 +0100

    app

---
+++ app.py
@@ -0,0 +1,13 @@
+from ddtrace import tracer
+import time
+
+
+def main():
+    while True:
+        with tracer.trace('main', service="jb-hackathon-2021-py") as span:
+            hello()
+
+def hello():
+    with tracer.trace('hello') as span:
+        time.sleep(5)
+        print("hello")
